bbc1cce9bb57a47819799a3f1d6866a15b4570be
pseudozach <git@pseudozach.com>
2024-02-10 20:43:50 -0800

    tx methods

--- src/repository/bitcoinCore.ts
+++ src/repository/bitcoinCore.ts
@@ -25,6 +25,7 @@ class BitcoinCoreClient {
   async getBlock(blockHash: string): Promise<any> {
     try {
       const result = await this.client.getBlock(blockHash);
+      console.log('getBlock result', result);
       return result;
     } catch (error) {
       throw new Error(`Error getBlock: ${error.message}`);
@@ -43,9 +44,29 @@ class BitcoinCoreClient {
   async getBlockHash(blockHeight: number): Promise<string> {
     try {
       const result = await this.client.getBlockHash(blockHeight);
+      console.log('getBlockHash result', result);
       return result;
     } catch (error) {
       throw new Error(`Error getBlockHash: ${error.message}`);
+    }
+  }
+
+  async getTransaction(txid: string): Promise<any> {
+    try {
+      const result = await this.client.getTransaction(txid);
+      console.log('getTransaction result', result);
+      return result;
+    } catch (error) {
+      throw new Error(`Error getTransaction: ${error.message}`);
+    }
+  }
+
+  async getRawTransaction(txid: string): Promise<string> {
+    try {
+      const result = await this.client.getRawTransaction(txid);
+      return result;
+    } catch (error) {
+      throw new Error(`Error getRawTransaction: ${error.message}`);
     }
   }
 }
